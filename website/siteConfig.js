9f80846ee58f13f82dc2e6584e3dfc21cbf2e7b7
MinHo Jung <anew0m@gmail.com>
2025-01-14 13:50:46 +0900

    docs: site url 수정

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -18,7 +18,7 @@ const users = [
 const siteConfig = {
   title: 'Test Site' /* title for your website */,
   tagline: 'A website for testing',
-  url: 'https://facebook.github.io' /* your website url */,
+  url: 'https://darling-tartufo-f70d81.netlify.app' /* your website url */,
   baseUrl: '/test-site/' /* base url for your project */,
   projectName: 'test-site',
   headerLinks: [
